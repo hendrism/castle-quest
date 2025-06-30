2d2096c4701b53c6b453f8ed64cc6b9d42bc3c5e
hendrism <hendrism@gmail.com>
2025-06-30 18:46:58 -0400

    Revamp header layout and visuals

--- constants.js
+++ constants.js
@@ -4,3 +4,12 @@ export const CONSTANTS = {
   LEVEL_XP_MULTIPLIER: 1.2,
   SEASON_CYCLE_MONTHS: 4
 };
+
+export const RESOURCE_COLORS = {
+  wood: '#27ae60',
+  stone: '#7f8c8d',
+  metal: '#95a5a6',
+  food: '#e67e22',
+  tools: '#3498db',
+  gems: '#9b59b6'
+};
